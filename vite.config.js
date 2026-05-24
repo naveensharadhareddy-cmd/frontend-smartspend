9dc51e0b55f600a7fb01643d1692d57aa9a3db36
naveensharadhaireddy-cmd <naveenreddysharadha@gmail.com>
2026-05-24 22:00:39 +0530

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash]-v2.js',
-        chunkFileNames: 'assets/[name]-[hash]-v2.js',
-      }
-    }
+  plugins: [react()]
+})
